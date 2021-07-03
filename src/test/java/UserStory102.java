6b63d33497942c18bcd83bb2d1b4daceab13508c
yohanna30 <yandemicael@yahoo.com>
2021-07-03 00:11:20 -0400

    User story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,2 +1,8 @@
 public class UserStory102 {
+    
+    //lot of code has been written here
+    //ready to push this branch to remote
+    //and open pull request so I can start collaboration
+    //so my team member can comment, review and approve or request for more changes
+
 }
